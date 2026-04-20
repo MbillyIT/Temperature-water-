9104879bb3947018213a8af93f6a2dfe427d10bc
Max <maxbillyeven2@gmail.com>
2026-04-20 15:41:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть температуру води");
+        string input = Console.ReadLine();
+        int temperature = int.Parse(input);
+
+        if (temperature <= 0)
+        {
+            Console.WriteLine("Лід");
+        }
+        else if (temperature >= 0 && temperature < 100)
+        {
+            Console.WriteLine("Тепло");
+        }
+        else if ( temperature >= 100)
+        {
+            Console.WriteLine("Пара");
+        }
+    }
+}
